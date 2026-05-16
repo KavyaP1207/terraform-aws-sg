aea160d68643060455dcab4d689d4168be86ccb7
kavya Poreddy <poreddykavya1207@gmail.com>
2026-05-16 11:55:47 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_security_group" "allow_tls" {
   name        = var.sg_name
-  description = var.description
+  description = var.sg_description
   vpc_id      = var.vpc_id
 
     egress {
